4fd8050d15624ada5ca66ab1f988a5c3ec0c3a44
hector-ramirez-cano <Zaph@no_mail.com>
2024-05-17 02:20:50 -0600

    Get cookie

--- Frontend/public/js/util.js
+++ Frontend/public/js/util.js
@@ -2,4 +2,32 @@ function isPowerOf2(value) {
     return (value & (value - 1)) === 0;
 }
 
-export { isPowerOf2 };
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+        end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+} 
+
+function isLoggedIn() {
+    let cookie = getCookie("oauth_token");
+
+    return cookie != null;
+}
+
+export { isPowerOf2, getCookie, isLoggedIn };
+
