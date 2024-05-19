fcef6dcc065b6168658efe2948e95f5f309e3b6c
hector-ramirez-cano <Zaph@no_mail.com>
2024-05-19 04:11:53 -0600

    OAuth Cookie expiration from token response

--- Backend/Rust/src/controller/api.rs
+++ Backend/Rust/src/controller/api.rs
@@ -1,5 +1,5 @@
 
-use rocket::{data::N, fs::{relative, NamedFile}, http::{Cookie, CookieJar, SameSite}, response::Redirect, serde::json::Value};
+use rocket::{data::N, fs::{relative, NamedFile}, http::{Cookie, CookieJar, SameSite}, response::Redirect, serde::json::Value, time::Duration};
 use rocket_oauth2::{OAuth2, TokenResponse};
 
 use crate::model::{db::{self, get_project_list}, types};
@@ -94,10 +94,15 @@ pub fn google_login(oauth2: OAuth2<Google>, cookies: &CookieJar<'_>) -> Redirect
 #[get("/auth/google")]
 pub fn google_auth_callback(token: TokenResponse<Google>, cookies: &CookieJar<'_>) -> Redirect {
     // Set a private cookie with the access token
+    let offset = token.expires_in().unwrap_or(0);
+    let offset = Duration::seconds(offset);
+    let offset = rocket::time::OffsetDateTime::now_utc().checked_add(offset);
+
     cookies.add(
         Cookie::build((OAUTH2_TOKEN_COOKIE, token.access_token().to_string()))
             .same_site(SameSite::Lax)
             .http_only(false)
+            .expires(offset)
             .build()
     );
     
